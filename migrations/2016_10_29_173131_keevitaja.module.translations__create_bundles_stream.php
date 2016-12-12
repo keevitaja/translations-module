893316b850c264f1018cd741dea2e53dfafc3623
Tanel Tammik <keevitaja@gmail.com>
2016-12-12 12:26:05 +0200

    remove config from bundle slug assignement

--- migrations/2016_10_29_173131_keevitaja.module.translations__create_bundles_stream.php
+++ migrations/2016_10_29_173131_keevitaja.module.translations__create_bundles_stream.php
@@ -27,10 +27,7 @@ class KeevitajaModuleTranslationsCreateBundlesStream extends Migration
             'required' => true
         ],
         'slug' => [
-            'required' => true,
-            'config' => [
-                'slugify' => 'name'
-            ]
+            'required' => true
         ]
     ];
 
